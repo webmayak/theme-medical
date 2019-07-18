5a384815581303a139985cf0887490e496e72354
Alex Kostin <web-mayak@mail.ru>
2019-07-18 22:42:04 +1000

    верстка карточки товаров

--- widgets/mainCatalog/views/_view.php
+++ widgets/mainCatalog/views/_view.php
@@ -19,13 +19,18 @@ $hasImage = $model->media && $model->media->issetMedia();
 
 ?><a href="<?= $model->present()->getUrl() ?>" class="catalog-item<?= $hasImage ? ' with-image' : ' no-image' ?>">
     <?php if ($hasImage): ?>
-    <div class="media-top">
-        <div class="block-img">
-            <?= Html::img($model->media->image(340, 276, false), ['alt'=>$model->name]); ?>
-        </div>
+    <div class="catalog-item__image">
+        <?= Html::img($model->media->image(340, 276, false), ['alt'=>$model->name]); ?>
     </div>
     <?php endif; ?>
-    <div class="catalog-item__footer">
+    <div class="catalog-item__info">
+        <div class="catalog-item__rating">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+        </div>
         <div class="catalog-item__title">
             <?= Html::encode($model->name) ?>
         </div>
@@ -33,6 +38,16 @@ $hasImage = $model->media && $model->media->issetMedia();
             Дженерик препарата: Sovaldi (Совальди)<br/>
             В упаковке: 28 Таблеток.<br/>
             Производитель: Hetero Индия
+        </div>
+        <div class="catalog-item__prices">
+            <div class="row">
+                <div class="col-xs-6">
+                    <span class="btn btn-primary">Купить</span>
+                </div>
+                <div class="col-xs-6">
+                    <span class="btn btn-primary">Купить</span>
+                </div>
+            </div>
         </div>
     </div>
 </a>
