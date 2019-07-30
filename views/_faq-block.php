99766c9febc7dac9a468559c668262669ff5f3fe
Alex Kostin <web-mayak@mail.ru>
2019-07-30 20:14:41 +1000

    оживил на главной странице блок "Вопрос-Ответ"

--- views/_faq-block.php
+++ views/_faq-block.php
@@ -1,5 +1,13 @@
-<div class="faq-block">
+<?php
+
+use common\modules\catalog\models\CatalogCategory;
+use yii\helpers\Html;
+
+$faqs = CatalogCategory::findOne(111)->getChildrenActive()->limit(4)->all();
+
+?><div class="faq-block">
 	<div class="row">
+		<?php foreach ($faqs as $faq): ?>
 		<div class="col-md-3">
 			<div class="faq-block-item">
 				<div class="faq-block-item__question">
@@ -7,7 +15,7 @@
 			    		Вопрос:
 					</div>
 					<div class="faq-block-item__question-text">
-			    		Начал принимать терапию сафосбувир даклатасфир и рибавирин на 3 день пожелтели склеры думаю это от рибавирина можно ли его отменить и продолжить терапию без него
+			    		<?= Html::encode($faq->name) ?>
 					</div>
 				</div>
 				<div class="faq-block-item__answer">
@@ -15,7 +23,7 @@
 			    		Ответ:
 					</div>
 					<div class="faq-block-item__answer-text">
-			    		Здравствуйте! Да, рибавирин может давать такой эффект. Отменить можно, но в этом случае следует увеличить продолжительность приема соф и дак до 24 недель.
+			    		<?= Html::encode($faq->description) ?>
 					</div>
 				</div>
 				<div class="faq-block-item__button">
@@ -23,74 +31,6 @@
 				</div>
 			</div>
 		</div>
-		<div class="col-md-3">
-			<div class="faq-block-item">
-				<div class="faq-block-item__question">
-					<div class="faq-block-item__question-title">
-			    		Вопрос:
-					</div>
-					<div class="faq-block-item__question-text">
-			    		Начал принимать терапию сафосбувир даклатасфир и рибавирин на 3 день пожелтели склеры думаю это от рибавирина можно ли его отменить и продолжить терапию без него
-					</div>
-				</div>
-				<div class="faq-block-item__answer">
-			    	<div class="faq-block-item__answer-title">
-			    		Ответ:
-					</div>
-					<div class="faq-block-item__answer-text">
-			    		Здравствуйте! Да, рибавирин может давать такой эффект. Отменить можно, но в этом случае следует увеличить продолжительность приема соф и дак до 24 недель.
-					</div>
-				</div>
-				<div class="faq-block-item__button">
-			    	<a href="/faq" class="btn btn-block btn-primary">Читать</a>
-				</div>
-			</div>
-		</div>
-		<div class="col-md-3">
-			<div class="faq-block-item">
-				<div class="faq-block-item__question">
-					<div class="faq-block-item__question-title">
-			    		Вопрос:
-					</div>
-					<div class="faq-block-item__question-text">
-			    		Начал принимать терапию сафосбувир даклатасфир и рибавирин на 3 день пожелтели склеры думаю это от рибавирина можно ли его отменить и продолжить терапию без него
-					</div>
-				</div>
-				<div class="faq-block-item__answer">
-			    	<div class="faq-block-item__answer-title">
-			    		Ответ:
-					</div>
-					<div class="faq-block-item__answer-text">
-			    		Здравствуйте! Да, рибавирин может давать такой эффект. Отменить можно, но в этом случае следует увеличить продолжительность приема соф и дак до 24 недель.
-					</div>
-				</div>
-				<div class="faq-block-item__button">
-			    	<a href="/faq" class="btn btn-block btn-primary">Читать</a>
-				</div>
-			</div>
-		</div>
-		<div class="col-md-3">
-			<div class="faq-block-item">
-				<div class="faq-block-item__question">
-					<div class="faq-block-item__question-title">
-			    		Вопрос:
-					</div>
-					<div class="faq-block-item__question-text">
-			    		Начал принимать терапию сафосбувир даклатасфир и рибавирин на 3 день пожелтели склеры думаю это от рибавирина можно ли его отменить и продолжить терапию без него
-					</div>
-				</div>
-				<div class="faq-block-item__answer">
-			    	<div class="faq-block-item__answer-title">
-			    		Ответ:
-					</div>
-					<div class="faq-block-item__answer-text">
-			    		Здравствуйте! Да, рибавирин может давать такой эффект. Отменить можно, но в этом случае следует увеличить продолжительность приема соф и дак до 24 недель.
-					</div>
-				</div>
-				<div class="faq-block-item__button">
-			    	<a href="/faq" class="btn btn-block btn-primary">Читать</a>
-				</div>
-			</div>
-		</div>
+		<?php endforeach; ?>
 	</div>
 </div>
