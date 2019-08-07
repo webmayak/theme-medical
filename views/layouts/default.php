0bec6b7b3693da1adab901f298738ad18b237ca6
Alex Kostin <web-mayak@mail.ru>
2019-08-08 02:43:57 +1000

    поменял meta theme-color

--- views/layouts/default.php
+++ views/layouts/default.php
@@ -29,7 +29,7 @@ $this->beginPage();
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="theme-color" content="#15C9CD">
+    <meta name="theme-color" content="#2bcd93">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
