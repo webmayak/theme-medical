134e1ca8f9cbfa2ba9b51014bf13750ec69b2c24
Alex Kostin <web-mayak@mail.ru>
2020-01-17 19:58:26 +1000

    добавил в шаблон скрипты Google Tag Manager

--- views/layouts/default.php
+++ views/layouts/default.php
@@ -37,8 +37,19 @@ $this->beginPage();
     <style>
         <?= Yii::$app->settings->get('css', 'default') ?>
     </style>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-WHXT354');</script>
+    <!-- End Google Tag Manager -->
 </head>
 <body class="page-<?= $_SERVER['REQUEST_URI'] == '/' ? 'front' : str_replace('/', '-', trim($_SERVER['REQUEST_URI'], '/')) ?>">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WHXT354"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <?php $this->beginBody() ?>
 <?= $this->render('_header') ?>
 <?= MegaMenu::widget() ?>
