0dc8a3fcbd95bab68849c23d6c0de4ec7c388a50
Alex Kostin <web-mayak@mail.ru>
2020-02-09 14:34:41 +1000

    изменил состав ссылок выводимых в /sitemap

--- views/site/sitemap.php
+++ views/site/sitemap.php
@@ -18,42 +18,33 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= $this->title ?>
 </h1>
 <div class="row">
-	<div class="col-md-4">
-		<div class="h2">Разделы</div>
-		<ul>
-		    <?php foreach ($links1 as $link) : ?>
-		        <li style="margin-top: 5px;">
-		            <?= Html::a($link['anchor'], $link['url']) ?>
-		        </li>
-		    <?php endforeach; ?>
-		</ul>
-		<div class="h2">Статьи</div>
-		<ul>
-		    <?php foreach ($links2 as $link) : ?>
-		        <li style="margin-top: 5px;">
-		            <?= Html::a($link['anchor'], $link['url']) ?>
-		        </li>
-		    <?php endforeach; ?>
-		</ul>
-	</div>
-	<div class="col-md-4">
-		<div class="h2">Каталог</div>
-		<ul>
-		    <?php foreach ($links3 as $link) : ?>
-		        <li style="margin-top: 5px;">
-		            <?= Html::a($link['anchor'], $link['url']) ?>
-		        </li>
-		    <?php endforeach; ?>
-		</ul>
-	</div>
-	<div class="col-md-4">
-		<div class="h2">Акции и скидки</div>
-		<ul>
-		    <?php foreach ($links4 as $link) : ?>
-		        <li style="margin-top: 5px;">
-		            <?= Html::a($link['anchor'], $link['url']) ?>
-		        </li>
-		    <?php endforeach; ?>
-		</ul>
-	</div>
+    <div class="col-md-4">
+        <div class="h2">Разделы</div>
+        <ul>
+            <?php foreach ($links1 as $link) : ?>
+                <li style="margin-top: 5px;">
+                    <?= Html::a($link['anchor'], $link['url']) ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+
+        <div class="h2">Каталог</div>
+        <ul>
+            <?php foreach ($links3 as $link) : ?>
+                <li style="margin-top: 5px;">
+                    <?= Html::a($link['anchor'], $link['url']) ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+    <div class="col-md-8">
+        <div class="h2">Статьи</div>
+        <ul>
+            <?php foreach ($links2 as $link) : ?>
+                <li style="margin-top: 5px;">
+                    <?= Html::a($link['anchor'], $link['url']) ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
 </div>
