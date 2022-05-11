6b01e0ecee1ea52bac49cd564405a9aa46b1db7f
Alex Kostin <web-mayak@mail.ru>
2022-05-12 03:31:44 +1000

    Текстовый блок на странице отзывов + рефакторинг

--- views/catalog/category/index-review-list.php
+++ views/catalog/category/index-review-list.php
@@ -1,6 +1,6 @@
 <?php
 
-use kop\y2sp\ScrollPager;
+use pantera\content\widgets\block\Block;
 use pantera\leads\widgets\form\LeadForm;
 use yii\data\ArrayDataProvider;
 use yii\web\View;
@@ -9,10 +9,15 @@ use yii\web\JsExpression;
 
 /* @var $this View */
 /* @var $dataProvider ActiveDataProvider */
+
 $this->title = 'Отзывы наших клиентов';
 $this->params['breadcrumbs'][] = $this->title;
 
 ?><h1 class="title-home"><?= Yii::$app->seo->getH1() ?></h1>
+
+<?= Block::widget([
+    'position' => 'reviews_page_top',
+]) ?>
 
 <?= ListView::widget([
     'dataProvider' =>  new ArrayDataProvider([
@@ -26,26 +31,11 @@ $this->params['breadcrumbs'][] = $this->title;
     'itemOptions' => [
         'class' => 'review-item',
     ],
-    // 'pager' => [
-    //     'class' => ScrollPager::class,
-    //     'item' => '.reviews',
-    //     'triggerText' => 'ПОКАЗАТЬ ЕЩЕ',
-    //     'noneLeftText' => '',
-    //     'enabledExtensions' => [
-    //         ScrollPager::EXTENSION_TRIGGER,
-    //         ScrollPager::EXTENSION_SPINNER,
-    //         ScrollPager::EXTENSION_NONE_LEFT,
-    //         ScrollPager::EXTENSION_PAGING,
-    //     ],
-    //     'triggerTemplate' => '<div class="ias-trigger" style="text-align: center; cursor: pointer;">
-    //                             <a class="btn btn-primary btn-lg">{text}</a>
-    //                         </div>',
-    //     'eventOnRendered' => new JsExpression('function(){initReviewReadMore();}'),
-    // ]
 ]) ?>
+
 <section class="review-form-wrapper">
-<?= LeadForm::widget([
-    'mode' => LeadForm::MODE_INLINE,
-    'key' => 'review',
-]) ?>
+    <?= LeadForm::widget([
+        'mode' => LeadForm::MODE_INLINE,
+        'key' => 'review',
+    ]) ?>
 </section>
